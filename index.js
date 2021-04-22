13aa99bd8c1608b6ca60b176a7f2410f62659ce3
KeslieBox <kesliebox@gmail.com>
2021-04-22 16:24:06 -0500

    commit tree 25a9396f0474701e0cf0390b553892e6105dc6ac

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 function superbowlWin(arrayObjects) {
     let results = arrayObjects.find(object => object.result === "W") 
 
-    return !!results ? results.year : undefined
+    // return !!results ? results.year : undefined
+    if (!!results) {
+       return results.year
+    } else {
+        return undefined
+    }
+    
 }
